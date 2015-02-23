4b4a2b8ced6152369692eaacf5a26108ba331430
Adnan Alvee <adnan.alvee@gmail.com>
2015-02-23 15:46:07 -0600

    First Commit

---
+++ Stacks&Queues/src/SimpleStack.java
@@ -0,0 +1,40 @@
+
+public class SimpleStack {
+
+	private int maxSize;         //size of stack array
+	private long[] stackArray;	 
+	private int top;             //top of the stack
+	
+	public SimpleStack(int s) {
+		maxSize = s;
+		stackArray = new long[maxSize];
+		top = -1;
+	}
+	
+	public void push() {
+		
+	}
+	
+	public void peek() {
+		
+	}
+	
+	public void pop() {
+		
+	}
+	
+	public void isEmpty() {
+		
+	}
+	
+	public void isFull() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
